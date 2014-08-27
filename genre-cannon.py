c2152873ffb07c9f19d8ce5e006036cd23327601
Thomas Gordon <tgord@mm.st>
2014-08-27 19:40:10 -0400

    Dict with top-level genres

--- genre-cannon.py
+++ genre-cannon.py
@@ -4,6 +4,7 @@
 
 from bs4 import BeautifulSoup
 import requests
+from pprint import pprint
 
 def find_some_child(tag):
     if tag.string is None:
@@ -11,35 +12,27 @@ def find_some_child(tag):
     else:
         return tag.child
 
-r = requests.get('https://en.wikipedia.org/wiki/List_of_popular_music_genres')
-soup = BeautifulSoup(r.text)
+r = requests.get(u'https://en.wikipedia.org/wiki/List_of_popular_music_genres')
+bowl = BeautifulSoup(r.text)
+cup = bowl.find('div', id='mw-content-text')
 
-exclude = ['Contents', 'Navigation Menu', 'Exclusions', 'References', 'Bibliography']
+exclude = [
+    u'Contents',
+    u'Navigation Menu',
+    u'Exclusions',
+    u'References',
+    u'Bibliography'
+]
 
-genres = {}
+top_level_genres = {}
 
-for tag in soup.find_all('h2'):
-    if tag.string is None:
-        tag = tag.child
-        #if (any(exclusion in tag.string for exclusion in exclude)):
-        #    continue
-        print tag.string
-        for child in tag.children:
-            if child.string is not None:
-                #if any(exclusion in child.string for exclusion in exclude):
-                #    continue
-                print child.string
-    #if tag.children is not None:
-        #if any(exclusion in tag.string for exclusion in exclude):
-        #    continue
-    #    print tag.child.string
-    #    print 'asdf'
-    #if tag.parent.name == 'h2':
-    #    genres[tag.string] = {}
-    #    for tag2 in tag.find_all('span', class_='mw-headline'):
-    #        if tag.parent.name == 'h2':
-    #            print(tag.string)
-    #if tag.parent.name == 'h3':
-        #print(tag.string)
-        #print(tag.parent.previous_sibling.previous_sibling['class'])
-       # #genres[tag.name.previous_sibling.previous_sibling.child.string] = tag.string
+for span_tag in cup.find_all('span', class_='mw-headline'):
+    if any(exclusion in span_tag.string for exclusion in exclude):
+        continue
+    if span_tag.parent.name == 'h2':
+        top_level_genres.setdefault(span_tag.string, {})
+        most_recent_h2 = span_tag.string
+    elif span_tag.parent.name == 'h3':
+        top_level_genres.setdefault(most_recent_h2, {}).setdefault(span_tag.string, {})
+
+pprint(top_level_genres, width=1)
