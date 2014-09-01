b5303fd7d21dd2a6afcaf8d175fc58de6ec92857
Thomas Gordon <ecommunity@teegee.mm.st>
2014-09-01 18:17:20 -0400

    Identifying start tag for subgenres, messily

--- genre-cannon.py
+++ genre-cannon.py
@@ -4,6 +4,20 @@ from bs4 import BeautifulSoup
 import requests
 from pprint import pprint
 
+def find_dict_depth(node, depth=0):
+    if not isinstance(node, dict) or not node:
+        return depth
+    return max(find_dict_depth(value, depth+1) for key, value in node.iteritems())
+
+def walk_dict(node):
+    for key, value in node.items():
+        if isinstance(value, dict):
+            if not value:
+                print('find '+key)
+            else:
+                print('find  '+key)
+                walk_dict(value)
+
 r = requests.get('https://en.wikipedia.org/wiki/List_of_popular_music_genres')
 bowl = BeautifulSoup(r.text)
 cup = bowl.find('div', id='mw-content-text')
@@ -23,8 +37,23 @@ for span_tag in cup.find_all('span', class_='mw-headline'):
         continue
     if span_tag.parent.name == 'h2':
         top_level_genres.setdefault(span_tag.string, {})
+        div_tag = span_tag.parent.next_sibling.next_sibling
+        class_key = div_tag.get('class')
+        if class_key is not None and 'hatnote' in class_key:
+            target_tag = div_tag.next_sibling.next_sibling
+        else:
+            target_tag = div_tag
+        print(target_tag.name)
+        #if not edit section, recurse lists, pass dict
         most_recent_h2 = span_tag.string
     elif span_tag.parent.name == 'h3':
         top_level_genres.setdefault(most_recent_h2, {}).setdefault(span_tag.string, {})
-
-pprint(top_level_genres, width=1)
+        div_tag = span_tag.parent.next_sibling.next_sibling
+        class_key = div_tag.get('class')
+        if class_key is not None and 'hatnote' in class_key:
+            target_tag = div_tag.next_sibling.next_sibling
+        else:
+            target_tag = div_tag
+        print(target_tag.name)
+        # if edit section, do stuff
+        # if not edit section, recurse lists, pass dict
